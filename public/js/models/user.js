07609e53388edbd1a1a6c5ae50314e8506a070e1
Patrick Fisher <patrick@pwfisher.com>
2014-02-25 15:24:05 -0800

    expand user model

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,8 +1,11 @@
 App.User = DS.Model.extend({
+  avatarUrl : DS.attr(),
+  bio: DS.attr('string'),
+  creationDate: DS.attr(),
   email: DS.attr('string'),
   firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
   fullName: function () {
     return this.get('firstName') + ' ' + this.get('lastName');
-  }.property('firstName', 'lastName')
+  }.property('firstName', 'lastName'),
+  lastName: DS.attr('string')
 });
