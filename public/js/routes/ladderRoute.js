d790006f6a9d88d1d8f943f246bce027496c0f76
Patrick Fisher <patrick@pwfisher.com>
2014-02-26 12:27:35 -0800

    close delete form on transition

--- public/js/routes/ladderRoute.js
+++ public/js/routes/ladderRoute.js
@@ -1,5 +1,11 @@
 App.LadderRoute = Ember.Route.extend({
   model: function (params) {
     return this.store.find('ladder', params.path);
+  },
+
+  actions: {
+    willTransition: function () {
+      this.controllerFor('ladder').send('cancelDelete');
+    }
   }
 });
--- public/js/routes/userRoute.js
+++ public/js/routes/userRoute.js
@@ -1,5 +1,11 @@
 App.UserRoute = Ember.Route.extend({
   model: function (params) {
     return this.store.find('user', params.user_id);
+  },
+
+  actions: {
+    willTransition: function () {
+      this.controllerFor('user').send('cancelDelete');
+    }
   }
 });
