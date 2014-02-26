40f82b079f1849e28b67c0bccbe9e330ac8182d6
Patrick Fisher <patrick@pwfisher.com>
2014-02-25 23:50:56 -0800

    basic layout markup and styles

--- public/js/router.js
+++ public/js/router.js
@@ -1,15 +1,21 @@
 App.Router.map(function () {
 
   this.resource('users', function () {
-    this.resource('user', { path: '/:user_id'}, function () {
+    this.resource('user', { path: '/:user_id' }, function () {
       this.route('edit');
     });
     this.route('create');
   });
 
   this.resource('ladders', function () {
-    this.resource('ladder', { path: '/:path'}, function () {
+    this.resource('ladder', { path: '/:path' }, function () {
       this.route('edit');
+      this.resource('games', function () {
+        this.resource('game', { path: '/:game_id' }, function () {
+          this.route('edit');
+        });
+        this.route('create');
+      });
     });
     this.route('create');
   });
