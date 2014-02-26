dd46df8f0bfef49f6cce61a402f0e45cd97bd755
Patrick Fisher <patrick@pwfisher.com>
2014-02-25 23:32:51 -0800

    use sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,17 +13,30 @@ module.exports = function(grunt) {
       }
     },
 
+    sass: {
+      dist: {
+        files: {
+          'public/app.css': [ 'scss/app.scss' ]
+        }
+      }
+    },
+
     watch: {
       emberTemplates: {
         files: 'public/js/templates/**/*.hbs',
         tasks: ['emberTemplates']
+      },
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-ember-templates');
 
   // Default task(s).
-  grunt.registerTask('default', ['emberTemplates']);
+  grunt.registerTask('default', ['emberTemplates', 'sass']);
 };
